5cd53e9fc11f98fd18d0d9fcedc17e6e66932d53
okapi <jf@geonef.fr>
2014-09-12 13:52:22 +0200

    sysconf/actual: nginx, php-fpm

--- actual/install.sh
+++ actual/install.sh
@@ -3,3 +3,11 @@
 . /usr/lib/sysconf.base/common.sh
 
 sysconf_require_packages nginx php5-fpm mysql-server
+
+rm -f /etc/nginx/sites-enabled/default
+
+if ps x | grep nginx | grep -vq grep; then
+    service nginx restart
+else
+    service nginx start
+fi
